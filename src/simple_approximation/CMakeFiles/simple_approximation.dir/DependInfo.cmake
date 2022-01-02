247aa58599c700b4b952e592578fd7ca6c64680e
logicp <logicp@logicp.ca>
2022-01-01 20:49:07 -0500

    initial commit

---
+++ src/simple_approximation/CMakeFiles/simple_approximation.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/data/stronglogic/opennn/examples/simple_approximation/main.cpp" "examples/simple_approximation/CMakeFiles/simple_approximation.dir/main.cpp.o" "gcc" "examples/simple_approximation/CMakeFiles/simple_approximation.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/data/stronglogic/opennn/opennn/CMakeFiles/opennn.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/simple_approximation/CMakeFiles/simple_approximation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_approximation.dir/main.cpp.o"
+  "CMakeFiles/simple_approximation.dir/main.cpp.o.d"
+  "simple_approximation"
+  "simple_approximation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_approximation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
